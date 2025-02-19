fb428f45361542f3d45ce3c35893f0391e6c8026
EchoPrograms <brogan.oberhaus30@gmail.com>
2025-02-19 00:24:47 -0500

    Command folder changes - BO

--- src/main/java/frc/robot/commands/SourceCoralIntake.java
+++ src/main/java/frc/robot/commands/SourceCoralIntake.java
@@ -2,11 +2,12 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.endeffector;
+package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.Constants;
 import frc.robot.commands.elevator.SetHeight;
+import frc.robot.commands.endeffector.EndEffectorIntakeCoral;
 import frc.robot.subsystems.EndEffector;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
